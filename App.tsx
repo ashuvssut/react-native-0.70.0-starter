2f8d0bf501cba6d3914c46cff005540f6df7a631
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-09-07 02:12:18 +0530

    init project lilac with ts template

---
+++ App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
