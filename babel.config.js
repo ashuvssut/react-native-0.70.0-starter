1fb68cc4c053643c386596ffdc9b66c1016353a9
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-09-07 18:20:17 +0530

    add babel-plugin-module-resolver=>absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ["module:metro-react-native-babel-preset"],
+	plugins: [
+		[
+			"module-resolver",
+			{
+				extensions: [
+					".js",
+					".jsx",
+					".ts",
+					".tsx",
+					".android.js",
+					".android.tsx",
+					".ios.js",
+					".ios.tsx",
+				],
+				root: ["./src"],
+				alias: {
+					$hooks: ["./src/theme", "./src/hooks"],
+					$types: ["./src/types/*"],
+					"$types/redux": ["./src/redux/types"],
+					$svg: ["./src/assets/svg/index"],
+					$utils: ["./src/utils/*"],
+				},
+			},
+		],
+	],
 };
