4881628f0f0aca3e1959bf00b18a3ea5467e77a6
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-09-07 16:48:15 +0530

    added flipper + redux + redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
+import { Text } from "react-native";
 
 const persistor = persistStore(store);
 
@@ -11,7 +12,7 @@ const Root = () => {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
-				<App />
+				<Text>adsfasdf</Text>
 			</PersistGate>
 		</Provider>
 	);
