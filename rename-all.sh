940d67e17c49f78c9f397dbadc961bd3f2db2b1a
Nikhil Mohite <79012023+nkilm@users.noreply.github.com>
2022-10-26 15:00:57 +0530

    add script to rename files recursively (#3)
    * add script to rename files recursively
    
    * minor changes
    
    Co-authored-by: Ashutosh Khanduala <ashu.khanduala@gmail.com>

---
+++ rename-all.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+query="lilac"
+
+if [ "$1" ]; then
+	replacement="$1"
+
+	echo "Renaming.."
+
+	find . -type f -name "*${query}*" | while read -r FILE; do
+		newfile=$(echo ${FILE} | sed -e "s/${query}/${replacement}/")
+		mv "${FILE}" "${newfile}"
+	done
+else
+	printf "Please provide you Project Name\n\n"
+	printf "example: ./rename-all.sh my-awesome-project\n\n"
+fi
